2a2a721675999853d27d3dc365c5f558c9f28daf
Fabricio Ribeiro <fabricio.ribeiro@outlook.com>
2024-09-25 23:00:59 +0100

    Create Database model for User. Added class method to add new user.

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 
+from enum import unique
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,3 +14,27 @@ def connect_db(app):
     db.init_app(app)
 
 # Database model will go here.
+
+class User(db.Model):
+    """User account database model
+
+    actions:
+        Create database structure to store User Accounts.
+    """
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+
+    @classmethod
+    def signup(cls, username, password, first_name, last_name, email):
+        """Class method to create new users in the database
+        """
+        new_user = User(username=username, password=password, first_name=first_name, last_name=last_name, email=email)
+        db.session.add(new_user)
+        db.session.commit
+        return new_user
