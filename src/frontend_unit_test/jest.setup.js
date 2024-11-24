8cdb99061b340d436f36726224349cd1394b4e81
Fabricio Ribeiro <fabricio.ribeiro@outlook.com>
2024-11-24 12:17:50 +0000

    Setup for Frontend testing

--- src/frontend_unit_test/jest.setup.js
+++ src/frontend_unit_test/jest.setup.js
@@ -1,13 +1,16 @@
 const { default: MockAdapter } = require("axios-mock-adapter");
 const axios = require("axios");
 const $ = require("jquery");
-const jestConfig = require("./jest.config");
 
 global.window = global;
 global.$ = $;
 global.jQuery = $;
 global.axios = axios;
 global.mockAxios = new MockAdapter(axios);
-global.Book = require("../static/book.js");
-
 jest.mock(`axios`);
+
+const { Book, Comment, Club, Message } = require("../static/classes.js");
+global.Book = Book;
+global.Comment = Comment;
+global.Club = Club;
+global.Message = Message;
